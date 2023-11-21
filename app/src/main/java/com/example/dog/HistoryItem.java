10a864071b15a7c642a3c2b51649e432644f59ae
aliensexistbro <10bit1023a@gmail.com>
2023-11-20 18:25:03 -0800

    history

--- app/src/main/java/com/example/dog/HistoryItem.java
+++ app/src/main/java/com/example/dog/HistoryItem.java
@@ -23,16 +23,15 @@ import com.google.android.material.navigation.NavigationBarView;
 
 public class HistoryItem extends AppCompatActivity {
 
+    // Shared Preferences
     private static final String PREF_NAME = "DogPrefs";
-
-    private static final String ACTIVITY_PREF_NAME = "ActivityPrefs";
     private String KEY_NAME = "name";
-    private ImageView pictureImageView;
 
+    // UI elements
+    private ImageView pictureImageView;
     private TextView logTitleTextView;
     private TextView titlePicTextView;
     private ImageButton historyItemButton;
-
     private BottomNavigationView appNavigation;
 
     @SuppressLint("Range")
@@ -40,49 +39,35 @@ public class HistoryItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history_item);
+
+        // Initialize UI elements
         logTitleTextView = findViewById(R.id.logTitleTextView);
         titlePicTextView = findViewById(R.id.pictureTitleTextView);
         pictureImageView = findViewById(R.id.pictureImageView);
-        displayWelcomeMessage();
-
         historyItemButton = findViewById(R.id.historyItemBackButton);
+
+        // Display welcome message and set up navigation
+        displayWelcomeMessage();
+        setUpNavigation();
+
+        // Back button click listener
         historyItemButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HistoryItem.this, History.class);
                 startActivity(intent);
-
-
             }
         });
-        appNavigation = findViewById(R.id.historyItemBottomNavigation);
-        appNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Intent intent;
-                if (item.getItemId() == R.id.homeMenuItem){
-                    intent = new Intent(HistoryItem.this, MainActivity.class);
-                    startActivity(intent);
-                }
-                if(item.getItemId() == R.id.historyMenuItem){
-                    intent = new Intent(HistoryItem.this, History.class);
-                    startActivity(intent);
-
-                }
-                if(item.getItemId() == R.id.profileMenuItem){
-                    intent = new Intent(HistoryItem.this, DogInfo.class);
-                    startActivity(intent);
-                }
-                return true;
-            }
-        });
-
     }
-    private void displayInfo(Cursor cursor1)
-    {
+
+    // Display user activity information
+    private void displayInfo(Cursor cursor1) {
+        // Retrieve selected date and fetch data from the database
         String selectedDate = getIntent().getStringExtra("SELECTED_DATE");
         MyDatabase myDatabase = new MyDatabase(this);
         Cursor cursor = myDatabase.getDataForDate(selectedDate);
+
+        // Display information if data is available
         if (cursor != null && cursor.getCount() > 0) {
             while (cursor.moveToNext()) {
                 cursor.moveToFirst();
@@ -104,51 +89,71 @@ public class HistoryItem extends AppCompatActivity {
         if (cursor != null) {
             cursor.close();
         }
-
     }
 
+    // Display activity information and set visibility based on count
     private void displayActivityInfo(int count, String key, int countTextViewId) {
         TextView countTextView = findViewById(countTextViewId);
-        // Set visibility based on count
         countTextView.setText(key + ": " + count);
         countTextView.setVisibility(count > 0 ? View.VISIBLE : View.GONE);
     }
 
+    // Display the photo if available
     private void displayPhoto(String photoPath) {
         pictureImageView.setImageURI(Uri.parse(photoPath));
     }
 
+    // Display welcome message based on user preferences
     private void displayWelcomeMessage() {
         SharedPreferences sharedPreferences = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
-        // Display name
         String selectedDate = getIntent().getStringExtra("SELECTED_DATE");
         String name = sharedPreferences.getString(KEY_NAME, "");
         logTitleTextView.setText(name + "'s Day " + selectedDate);
         MyDatabase myDatabase = new MyDatabase(this);
         myDatabase.printAllData();
         Cursor cursor = myDatabase.getDataForDate(selectedDate);
-        Log.d("cursor count ", "girl" + cursor.getCount() +selectedDate);
+
         if (cursor != null && cursor.getCount() > 0) {
             cursor.moveToFirst();
             @SuppressLint("Range") String photoPath = cursor.getString(cursor.getColumnIndex(Constants.PHOTO_PATH));
-            if(!photoPath.equals("none")){
-                Log.d("photo", photoPath);
+            if (!photoPath.equals("none")) {
                 displayPhoto(photoPath);
                 titlePicTextView.setText("Picture of the Day");
-            } else
-            {
+            } else {
                 titlePicTextView.setText("No Picture of the Day");
             }
             displayInfo(cursor);
         } else {
-            // Handle case where there is no data for today
             titlePicTextView.setText("No Entry");
             pictureImageView.setVisibility(View.GONE);
         }
 
-        // Close the cursor to free up resources
         if (cursor != null) {
             cursor.close();
         }
+    }
+
+    // Set up bottom navigation
+    private void setUpNavigation() {
+        appNavigation = findViewById(R.id.historyItemBottomNavigation);
+        appNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent;
+                if (item.getItemId() == R.id.homeMenuItem) {
+                    intent = new Intent(HistoryItem.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                if (item.getItemId() == R.id.historyMenuItem) {
+                    intent = new Intent(HistoryItem.this, History.class);
+                    startActivity(intent);
+                }
+                if (item.getItemId() == R.id.profileMenuItem) {
+                    intent = new Intent(HistoryItem.this, DogInfo.class);
+                    startActivity(intent);
+                }
+                return true;
+            }
+        });
     }
 }
